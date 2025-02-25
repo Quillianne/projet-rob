eb0407176dd34db6a7a0caeda1b64491fbb869dc
yasmine <yasmine.raoux@ensta.fr>
2025-02-25 15:40:50 +0100

    test obstacle

--- dev/api/openai/test_vision.py
+++ dev/api/openai/test_vision.py
@@ -9,7 +9,7 @@ def encode_image(image_path):
         return base64.b64encode(image_file.read()).decode("utf-8")
 
 # Path to your image
-image_path = "./raw_color.png"
+image_path = "photos/obstacle_1.png"
 file_prompt = open("prompt.txt", "r")
 prompt = file_prompt.read()
 
